9e64baafd7587331c2efd3ccdec9a433a0218e20
atrislab <atrislab@gmail.com>
2023-07-05 13:53:34 +0200

    rough sketch v0.1

--- weatherapp/src/App.jsx
+++ weatherapp/src/App.jsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react"; // Importa React y los hooks useState y useEffect
+import axios from "axios"; // Importa Axios para hacer solicitudes HTTP
+import { v4 as uuidv4 } from "uuid"; // Importa UUID para generar claves únicas
 
-function App() {
-  const [count, setCount] = useState(0)
+const WeatherList = () => {
+  // Define el componente WeatherList
+  const [weatherData, setWeatherData] = useState([]); // Crea un estado para almacenar los datos meteorológicos
+
+  useEffect(() => {
+    // Usa el hook useEffect para hacer una solicitud HTTP cuando se monta el componente
+    const fetchData = async () => {
+      // Define una función asíncrona para hacer la solicitud HTTP
+      const result = await axios(
+        // Usa Axios para hacer la solicitud HTTP a la API de OpenWeatherMap
+        "https://api.openweathermap.org/data/2.5/forecast?q=Madrid&appid=aqui_va_la_appkey"
+      );
+      setWeatherData(result.data.list); // Actualiza el estado con los datos meteorológicos obtenidos de la API
+    };
+    fetchData(); // Llama a la función fetchData para hacer la solicitud HTTP
+  }, []); // Pasa un array vacío como segundo argumento para que useEffect solo se ejecute una vez
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {weatherData.map(
+        (
+          item // Recorre los datos meteorológicos y renderiza un componente WeatherCard para cada uno
+        ) => (
+          <WeatherCard key={uuidv4()} item={item} />
+        )
+      )}
+    </div>
+  );
+};
 
-export default App
+const WeatherCard = ({ item }) => {
+  // Define el componente WeatherCard
+  const { dt_txt, main, weather } = item; // Extrae las propiedades necesarias del objeto item
+  const date = new Date(dt_txt); // Crea un objeto Date a partir de la propiedad dt_txt
+
+  return (
+    <div>
+      <h3>{date.toLocaleDateString()}</h3>
+      <p>Time: {date.toLocaleTimeString()}</p>
+      <p>Temperature: {main.temp}</p>
+      <p>Weather: {weather[0].description}</p>
+    </div>
+  );
+};
+
+const WeatherForm = () => {
+  // Define el componente WeatherForm
+  const [city, setCity] = useState(""); // Crea un estado para almacenar el nombre de la ciudad
+
+  const handleSubmit = (e) => {
+    // Define una función manejadora para el evento submit del formulario
+    e.preventDefault(); // Previene que se envíe el formulario por defecto
+    console.log(city); // Muestra el nombre de la ciudad en la consola del navegador
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {" "}
+      <input
+        type="text"
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+const App = () => {
+  // Define el componente App que renderiza los componentes WeatherForm y WeatherList
+  return (
+    <div>
+      <WeatherForm />
+      <WeatherList />
+    </div>
+  );
+};
+
+export default App; // Exporta el componente App por defecto para que pueda ser usado en otros archivos
